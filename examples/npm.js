fc18a1378bda15cb6ff71a314a6a0e171bb664db
Dominic Tarr <dominic.tarr@gmail.com>
2012-07-23 09:05:30 +1200

    add pausing to example

--- examples/npm.js
+++ examples/npm.js
@@ -1,13 +1,16 @@
 var probe = require('..')()
 var request = require('request')
 var through = require('through')
+var tester = require('stream-tester')
 
 request('http://isaacs.iriscouch.com/registry/_all_docs')
   .pipe(probe.createProbe())
+  .pipe(tester.createPauseStream(0.001, 100))
   .on('end', function () {
     probe.end()
   })
-  .pipe(process.stdout, {end: false})
+  
+//  .pipe(process.stdout, {end: false})
 probe.pipe(through(function (data) {
   console.error(data)
 }))
